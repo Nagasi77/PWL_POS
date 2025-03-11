c8a6a48553936f91d8aff90970d6e3ee7d2eb9c7
Much. Zannur Nadhif <muchzannurnadhif@gmail.com>
2025-03-11 22:00:12 +0700

    Jobsheet5Tugas_Kategori

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,13 @@ Route::prefix('level')->group(function () {
     Route::put('/{id}', [LevelController::class, 'update']);
     Route::delete('/{id}', [LevelController::class, 'destroy']);
 });
+
+Route::prefix('kategori')->group(function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list'])->name('kategori.list');
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
