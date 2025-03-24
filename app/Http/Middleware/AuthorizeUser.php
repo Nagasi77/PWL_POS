f2212b29fe54f320353ea5ece54ef243526a1e36
Much. Zannur Nadhif <muchzannurnadhif@gmail.com>
2025-03-24 17:39:11 +0700

    Jobsheet7-Praktikum3-&-Tugas3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -8,13 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeUser
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        return $next($request);
+        $user_role = $request->user()->getRole();
+
+        if (in_array($user_role, $roles)) {
+            return $next($request);
+        }
+
+        abort(403, 'Forbidden. You are not authorized to access this page.');
     }
 }
